9b0f2c54bb0cdcd3d14f226076f8ab5dd822577e
Alexander Gómez <gomezalextremo@gmail.com>
2024-04-19 11:58:50 -0500

    cambio en lab_2

---
+++ Estructura_de_datos/Laboratorio_2/Program.cs
@@ -0,0 +1,12 @@
+﻿//  programa que sume los n primeros numeros
+
+var n = 10;
+var suma = 0;
+
+for (var i = 0; i <= n; i++)
+{
+    suma = suma + i;
+}
+
+
+Console.WriteLine(" lA SUMA ES :" + suma.ToString());
