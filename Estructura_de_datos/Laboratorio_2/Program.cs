b16a399dbeba09ae0a10aabe71f8997ca8e5c285
Alexander Gómez <gomezalextremo@gmail.com>
2024-04-21 14:22:13 -0500

    ejercicios

--- Estructura_de_datos/Laboratorio_2/Program.cs
+++ Estructura_de_datos/Laboratorio_2/Program.cs
@@ -1,12 +1,54 @@
 ﻿//  programa que sume los n primeros numeros
 
-var n = 50;
-var suma = 0;
+//var n = 50;
+//var suma = 0;
 
-for (var i = 0; i <= n; i++)
+//for (var i = 0; i <= n; i++)
+//{
+//    suma = suma + i;
+//}
+
+
+//Console.WriteLine($" La suma de {n} primero numeros : {suma}");
+
+
+///*1) Realiza la suma, resta, multiplicación y división de dos números ingresados por el usuario.  
+// */
+
+//using System;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // Solicitar al usuario que ingrese dos números
+//        Console.WriteLine("Ingrese el primer número:");
+//        double num1 = Convert.ToDouble(Console.ReadLine());
+
+//        Console.WriteLine("Ingrese el segundo número:");
+//        double num2 = Convert.ToDouble(Console.ReadLine());
+
+//        // Realizar las operaciones matemáticas
+//        double suma = num1 + num2;
+//        double resta = num1 - num2;
+//        double multiplicacion = num1 * num2;
+//        double division = num1 / num2;
+
+//        // Mostrar los resultados
+//        Console.WriteLine($"La suma de {num1} y {num2} es: {suma}");
+//        Console.WriteLine($"La resta de {num1} y {num2} es: {resta}");
+//        Console.WriteLine($"La multiplicación de {num1} y {num2} es: {multiplicacion}");
+//        Console.WriteLine($"La división de {num1} entre {num2} es: {division}");
+
+//        Console.ReadLine(); // Esperar a que el usuario presione Enter para salir
+//    }
+//}
+
+//2) Solicita un número al usuario y determina si es par o impar.  
+
+using System;
+
+class Program
 {
-    suma = suma + i;
-}
-
-
-Console.WriteLine($" La suma de {n} primero numeros : {suma}");
+    static void Main(string[] args)
+    {
