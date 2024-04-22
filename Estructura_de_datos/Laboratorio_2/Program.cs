8200b09ff5636abc47cc7fad3f5de8d16fa2c90b
Alexander Gómez <gomezalextremo@gmail.com>
2024-04-22 18:10:22 -0500

    arrays string

--- Estructura_de_datos/Laboratorio_2/Program.cs
+++ Estructura_de_datos/Laboratorio_2/Program.cs
@@ -1,4 +1,4 @@
-﻿//  programa que sume los n primeros numeros
+﻿//programa que sume los n primeros numeros
 
 //var n = 50;
 //var suma = 0;
@@ -9,46 +9,37 @@
 //}
 
 
-//Console.WriteLine($" La suma de {n} primero numeros : {suma}");
+//Console.WriteLine($" la suma de {n} primero numeros : {suma}");
 
 
-///*1) Realiza la suma, resta, multiplicación y división de dos números ingresados por el usuario.  
-// */
+/*1) realiza la suma, resta, multiplicación y división de dos números ingresados por el usuario.  
+ */
 
 //using System;
-
 //class Program
 //{
 //    static void Main(string[] args)
 //    {
-//        // Solicitar al usuario que ingrese dos números
-//        Console.WriteLine("Ingrese el primer número:");
+//        // solicitar al usuario que ingrese dos números
+//        Console.WriteLine("ingrese el primer número:");
 //        double num1 = Convert.ToDouble(Console.ReadLine());
 
-//        Console.WriteLine("Ingrese el segundo número:");
+//        Console.WriteLine("ingrese el segundo número:");
 //        double num2 = Convert.ToDouble(Console.ReadLine());
 
-//        // Realizar las operaciones matemáticas
+//        // realizar las operaciones matemáticas
 //        double suma = num1 + num2;
 //        double resta = num1 - num2;
 //        double multiplicacion = num1 * num2;
 //        double division = num1 / num2;
 
-//        // Mostrar los resultados
-//        Console.WriteLine($"La suma de {num1} y {num2} es: {suma}");
-//        Console.WriteLine($"La resta de {num1} y {num2} es: {resta}");
-//        Console.WriteLine($"La multiplicación de {num1} y {num2} es: {multiplicacion}");
-//        Console.WriteLine($"La división de {num1} entre {num2} es: {division}");
+//        // mostrar los resultados
+//        Console.WriteLine($"la suma de {num1} y {num2} es: {suma}");
+//        Console.WriteLine($"la resta de {num1} y {num2} es: {resta}");
+//        Console.WriteLine($"la multiplicación de {num1} y {num2} es: {multiplicacion}");
+//        Console.WriteLine($"la división de {num1} entre {num2} es: {division}");
 
-//        Console.ReadLine(); // Esperar a que el usuario presione Enter para salir
+//        Console.ReadLine(); // esperar a que el usuario presione enter para salir
 //    }
 //}
 
-//2) Solicita un número al usuario y determina si es par o impar.  
-
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
---
+++ Estructura_de_datos/Laboratorio_2/Program1.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Laboratorio_2
+//{
+//    internal class Program1
+//    {
+//    }
+//}
+
+
+String cadena = "clase";
+
+char[] cadena2 = ['c', 'l', 'a', 's', 'e'];
+
+Console.WriteLine(cadena2);
+
